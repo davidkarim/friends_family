f3a284e61257cd063b8d36c36596b079672f55e8
David Karim <davidkarim@gmail.com>
2016-04-30 14:59:30 -0400

    obj using create to welcome controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,8 +5,9 @@ class WelcomeController < ApplicationController
 
 
     s3 = Aws::S3::Resource.new(region:'us-east-1')
-    obj = s3.bucket('food-trucks').object(key: 'testing-1')
-    obj.upload_file(@file)
+    #obj = s3.bucket('food-trucks').object(key: 'testing-1')
+    obj = s3.bucket('food-trucks').objects.create("testing.jpg", @file.tempfile)
+    #obj.upload_file(@file)
 
   end
 end

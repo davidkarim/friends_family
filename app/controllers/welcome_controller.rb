0c4ca8e9c0f164eeb9218ceddd5a9b1a979a675b
David Karim <davidkarim@gmail.com>
2016-05-08 10:49:01 -0400

    Remove upload test from welcome controller. Fix view for trucks with no schedules.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,4 @@
 class WelcomeController < ApplicationController
-  def upload
-    @testing = "This is currently a Test"
-    @file = params[:picture]
-
-
-    s3 = Aws::S3::Resource.new(region:'us-east-1')
-    obj = s3.bucket('food-trucks').object('filename.jpg')
-    obj.upload_file(@file.tempfile)
-
+  def index
   end
 end
